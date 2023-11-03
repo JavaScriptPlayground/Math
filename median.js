a0669122dca0cdc53fce66cd8057dc1d2381a54e
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-11-03 09:28:23 +0100

    Create median.js

---
+++ median.js
@@ -0,0 +1,22 @@
+/**
+ * This function calculates the median value for an array of numbers.
+ * 
+ * @function
+ * @param {number[]} array List of numbers
+ * @returns {number} The median value
+ */
+export function median(array) {
+    if (array.length === 0) {
+        return 0;
+    }
+
+    const isOdd = ((array.length % 2) === 1);
+
+    array.sort((a, b) => a - b);
+    
+    if (isOdd) {
+        return array[(array.length + 1) / 2];
+    } 
+    
+    return (array[(array.length / 2) - 1] + array[array.length / 2]) / 2
+}
